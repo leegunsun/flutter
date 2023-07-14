5f08d3fb6b04b18c18ccf53ba3caf794afb2ec66
leegunsun <talkcjstk@naver.com>
2023-07-14 12:30:06 +0900

    flutter provider 학습

---
+++ provider_overview_05/lib/models/dog.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/cupertino.dart';
+
+class Dog extends ChangeNotifier {
+  final String name;
+  final String breed;
+  int age;
+  Dog({
+    required this.name,
+    required this.breed,
+    this.age = 1,
+  });
+
+  void grow() {
+    age++;
+    notifyListeners();
+  }
+}
